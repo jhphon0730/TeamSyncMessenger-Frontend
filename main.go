1800de629cf4bc6251483fb67ffd5f7816acae70
Naithar01 <eeung025@gmail.com>
2023-11-30 15:21:39 +0900

    * Initial project Setting

--- main.go
+++ main.go
@@ -2,10 +2,12 @@ package main
 
 import (
 	"embed"
+	"log"
 
 	"github.com/wailsapp/wails/v2"
 	"github.com/wailsapp/wails/v2/pkg/options"
 	"github.com/wailsapp/wails/v2/pkg/options/assetserver"
+	"github.com/wailsapp/wails/v2/pkg/options/windows"
 )
 
 //go:embed all:frontend/dist
@@ -17,20 +19,26 @@ func main() {
 
 	// Create application with options
 	err := wails.Run(&options.App{
-		Title:  "TeamSyncMessenger-Frontend",
+		Title:  "TeamSyncMessenger",
 		Width:  1024,
 		Height: 768,
 		AssetServer: &assetserver.Options{
 			Assets: assets,
 		},
-		BackgroundColour: &options.RGBA{R: 27, G: 38, B: 54, A: 1},
+		BackgroundColour: &options.RGBA{R: 0, G: 0, B: 0, A: 0},
 		OnStartup:        app.startup,
 		Bind: []interface{}{
 			app,
 		},
+		Windows: &windows.Options{
+			WindowIsTranslucent:  true,
+			WebviewIsTransparent: true,
+			BackdropType:         windows.Mica,
+			Theme:                windows.SystemDefault,
+		},
 	})
 
 	if err != nil {
-		println("Error:", err.Error())
+		log.Fatal(err.Error())
 	}
 }
