66c2a3ebd8535039d622399d12e584e7b330ec7b
Naithar01 <eeung025@gmail.com>
2023-12-04 15:58:04 +0900

    * Add RegisterPage Handler

--- frontend/src/pages/user/RegisterPage.tsx
+++ frontend/src/pages/user/RegisterPage.tsx
@@ -2,17 +2,37 @@ import React from "react";
 import { Fragment } from "react";
 import { Link } from "react-router-dom";
 
+import { userModels } from "../../models/user/user.models";
 import styles from "../../styles/pages/user/login.module.css"
 import logo from "../../assets/images/logo.png"
 
 const RegisterPage = () => {
-  const handleSignUp = () => {
-    console.log("회원가입 버튼이 클릭되었습니다.");
+  const [registerState, setRegisterState] = React.useState<userModels.RegisterUserModel>({
+    username: "",
+    password: "",
+    password_check: "",
+    email: "",
+  })
+
+  const ChangeRegisterStateHandler = ( event: React.ChangeEvent<HTMLInputElement> ) => {
+    const { name, value } = event.target;
+
+    setRegisterState((prev) => {
+      return {
+        ...prev, [name]: value 
+      }
+    });
+  };
+
+  const SubmitLoginHandler = ( event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+    console.log(registerState);
   };
 
   return (
     <Fragment>
-      <form className={styles.login_container}>
+      <form className={styles.login_container} onSubmit={SubmitLoginHandler}>
         <div className={styles.login_content}>
           <img
             src={logo}
@@ -26,25 +46,37 @@ const RegisterPage = () => {
             <input
               type="text"
               placeholder="사용자명"
+              name="username"
               className={styles.input}
+              value={registerState.username}
+              onChange={ChangeRegisterStateHandler}
             />
             <input
               type="password"
               placeholder="비밀번호"
+              name="password"
               className={styles.input}
+              value={registerState.password}
+              onChange={ChangeRegisterStateHandler}
             />
             <input
               type="password"
               placeholder="비밀번호 확인 재입력"
+              name="password_check"
               className={styles.input}
+              value={registerState.password_check}
+              onChange={ChangeRegisterStateHandler}
             />
             <input
               type="email"
               placeholder="이메일"
+              name="email"
               className={styles.input}
+              value={registerState.email}
+              onChange={ChangeRegisterStateHandler}
             />
           </div>
-          <button type="submit" className={styles.login_button} onClick={handleSignUp}>
+          <button type="submit" className={styles.login_button}>
             회원가입
           </button>
           <div className={styles.signup_container}>
