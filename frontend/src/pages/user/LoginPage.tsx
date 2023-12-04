f7dbd7b353514d24c1847c5341fae2664ea10fc7
Naithar01 <eeung025@gmail.com>
2023-12-04 11:53:10 +0900

    * Add Login Page Button & CSS

--- frontend/src/pages/user/LoginPage.tsx
+++ frontend/src/pages/user/LoginPage.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Fragment } from "react";
+import { Link } from "react-router-dom";
 
 import styles from "../../styles/pages/user/login.module.css"
 
@@ -10,14 +11,32 @@ const LoginPage: React.FC = () => {
 
   return (
     <Fragment>
-      <div className={styles.telegram_login_container}>
-        <div className={styles.telegram_login_content}>
-          <p className={styles.telegram_login_text}>
+      <div className={styles.login_container}>
+        <div className={styles.login_content}>
+          <p className={styles.login_text}>
             로그인하세요.
           </p>
-          <button className={styles.telegram_login_button} onClick={handleLogin}>
+          <div className={styles.input_container}>
+            <input
+              type="text"
+              placeholder="사용자명"
+              className={styles.input}
+            />
+            <input
+              type="password"
+              placeholder="비밀번호"
+              className={styles.input}
+            />
+          </div>
+          <button className={styles.login_button} onClick={handleLogin}>
             로그인
           </button>
+          <div className={styles.signup_container}>
+            <span>계정이 없으신가요?</span>
+            <Link to="/signup" className={styles.signup_link}>
+              회원가입
+            </Link>
+          </div>
         </div>
       </div>
     </Fragment>
