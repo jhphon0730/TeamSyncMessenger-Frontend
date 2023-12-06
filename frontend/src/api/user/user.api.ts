961c30505ba747567622da241577915c54e7de43
Naithar01 <eeung025@gmail.com>
2023-12-06 13:59:41 +0900

    * Edit Register Fetch Functions

--- frontend/src/api/user/user.api.ts
+++ frontend/src/api/user/user.api.ts
@@ -1,37 +1,24 @@
 import { userModels } from "../../models/user/user.models";
 import { Response } from "../../models/helper/helper.module";
 
-export const RegisterUser = async ({username, password, email}: userModels.RegisterUserModel): Promise<Response<userModels.UserModel> | Response<object>> => {
-  let myHeaders: HeadersInit = new Headers();
-  myHeaders.append("Content-Type", "application/json");
-  
-  let raw = JSON.stringify({
-    "username": username,
-    "email": email,
-    "password": password
-  });
-
-  try {
-    const response = await fetch("/api/user/register/", {
-      method: 'POST',
-      headers: myHeaders,
-      body: raw,
-      redirect: 'follow'
-    });
-
-    const result: userModels.UserModel = await response.json();
-    
-    const responseData: Response<userModels.UserModel> = {
-      Status: true,
-      Message: "Success",
-      Error: null, 
-      Data: result,
-    };
-
-    return responseData;
-  } catch (error) {
-    console.error('Error during registration:', error);
-
+export const RegisterUser = async ({username, password, email}: userModels.RegisterUserModel): Promise<Response<userModels.UserModel> | Response<object>> => { 
+  await fetch("http://192.168.0.48:8080/api/user/register/", {
+    method: 'POST',
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      "username": username,
+      "email": email,
+      "password": password
+    }),
+    redirect: 'follow'
+  })
+  .then(async (res) => await res.json())
+  .then((data: Response<userModels.UserModel>) => {
+    return data
+  })
+  .catch((error) => {
     const errorResponse: Response<object> = {
       Status: false,
       Message: "Error during registration",
@@ -40,5 +27,6 @@ export const RegisterUser = async ({username, password, email}: userModels.Regis
     };
 
     return errorResponse;
-  }
+  })
+  return {} as Response<object>;
 };
